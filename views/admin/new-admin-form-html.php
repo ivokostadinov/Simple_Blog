bf99ec137c6c03a72c071815cfc43d8170aca16e
ivokostadinov <ivokostadinov19@gmail.com>
2015-03-12 17:41:42 +0200

    new admin user form

--- views/admin/new-admin-form-html.php
+++ views/admin/new-admin-form-html.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ivo
- * Date: 12.3.2015 г.
- * Time: 10:46
- */
+if (isset($adminFormMessage) === false) {
+    $adminFormMessage = "";
+}
+
+return "<form method='post' action='admin.php?page=users'>
+    <fieldset>
+        <legend>Create new admin user</legend>
+        <label>e-mail</label>
+        <input type='text' name='email' required/>
+        <label>password</label>
+        <input type='password' name='password' required/>
+        <input type='submit' value='create user' name='new-admin'/>
+    </fieldset>
+    <p id='admin-form-message'>$adminFormMessage</p>
+</form>
+";
