a087aab1b95da53fd63258a78ab76aa08e983afd
ivokostadinov <ivokostadinov19@gmail.com>
2015-03-12 17:38:38 +0200

    admin navigation

--- views/admin/admin-navigation.php
+++ views/admin/admin-navigation.php
@@ -4,4 +4,5 @@ return "
 <nav id = 'admin-navigation'>
     <a href='admin.php?page=entries'>ALL entries</a>
     <a href='admin.php?page=editor'>Editor</a>
+    <a href='admin.php?page=users'>Create admin user</a>
 </nav>";
